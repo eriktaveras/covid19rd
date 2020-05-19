28884b3b7c8e16d15382d69f0c695a813c4d453f
Erik Taveras <eriktaveras@gmail.com>
2020-05-19 00:43:07 -0400

    Implementacion de las noticias automaticas

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ from bs4 import BeautifulSoup
 
 
 app = Flask(__name__)
-@app.route('/', methods=["GET", "POST"])
+@app.route('/')
 def home():
         url = "https://news.google.com/covid19/map?hl=en-US&gl=US&ceid=US%3Aen&mid=%2Fm%2F027rn"
         page = requests.get(url)
         soup = BeautifulSoup(page.content, 'html.parser')
         data = []
         paises=[]
+       
         table = soup.find('table', class_='pH8O4c')
         table_body = table.find('tbody')
         rows= table_body.find_all('tr')
@@ -25,14 +26,38 @@ def home():
                 nombre = row.find_all("div", { "class" : "pcAJd" })
                 nombre = [ele.text.strip() for ele in nombre]
                 paises.append([ele for ele in nombre if ele])
-        return render_template('index.html', data=data, len = len(data), paises=paises)
+        return render_template('home.html', data=data, len = len(data), paises=paises)
 
-#@app.route('/get_data')
-# def get_data():
-#  labels = paises
-#  data = data
-#  return flask.jsonify({'payload':json.dumps({'data':data, 'labels':labels})})
         
-     
+@app.route('/ultimas')
+def noticias():
+        url = "https://news.google.com/covid19/map?hl=en-US&gl=US&ceid=US%3Aen&mid=%2Fm%2F027rn"
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        weblinks = soup.find_all('article')
+        pagelinks = []
+        linkeable = []
+        
+        base_url = 'https://news.google.com/'
+        
+        
+        for item in weblinks:
+                de = []
+                product_name = item.find("a", {"class":"VDXfz"}) 
+                product_link = 'https://news.google.com/' + str(product_name.get('href'))
+                product_name = product_name.text.replace('\n', "").strip() 
+                linkeable.append(product_link)
+
+        for link in weblinks:    
+                url = link.find_all('h4')
+                text =[ele.text.strip() for ele in url]
+                enlace = link.find_all('a', {"class" : "hXwDdf"})
+                final = [ele.text.strip() for ele in enlace]    
+                pagelinks.append(text)
+
+
+        return render_template("ultimas.html", pagelinks=pagelinks, len=len(pagelinks), linkeable=linkeable, lendk=len(linkeable), de=de)
+
+
 if __name__ == "__main__": 
-        app.run()
+        app.run(port=5000, debug=True, threaded=True)
